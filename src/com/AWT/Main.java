f1e8cb2d9096105b3b5d22cedd24e6c96cd51693
timofeya123 <timofeya123@inbox.ru>
2021-06-02 20:08:48 +0600

    Desktop_comment

--- src/com/AWT/Main.java
+++ src/com/AWT/Main.java
@@ -9,15 +9,19 @@ import java.net.URISyntaxException;
 public class Main {
 
     public static void main(String[] args) throws IOException, URISyntaxException {
-        if (Desktop.isDesktopSupported()){
-            Desktop desktop = Desktop.getDesktop();
-            for (Desktop.Action action:Desktop.Action.values()){
+        if (Desktop.isDesktopSupported()){ // проверка, что можно вызывать приложения из вне
+
+            Desktop desktop = Desktop.getDesktop(); // добавляем переменную для работы с внешним миром (программами)
+
+            for (Desktop.Action action:Desktop.Action.values()){ // цикл всех действия с внешним миром (программами)
                 System.out.println(action);
             }
-            if (desktop.isSupported(Desktop.Action.OPEN)){
+
+            if (desktop.isSupported(Desktop.Action.OPEN)){ // открыть картинку
                 desktop.open(new File("enot.jpeg"));
             }
-            if (desktop.isSupported(Desktop.Action.BROWSE)){
+
+            if (desktop.isSupported(Desktop.Action.BROWSE)){ // открыть ссылку
                 desktop.browse(new URI("https://google.com"));
             }
         }
